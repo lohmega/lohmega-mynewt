d79f561e592a5faea358778e7f2524faea12af88
Simon Frank <521620+5frank@users.noreply.github.com>
2021-04-21 14:51:49 +0200

    sgp40 missing BUS_DRIVER macro

--- hw/drivers/sensors/sgp40/include/sgp40/sgp40.h
+++ hw/drivers/sensors/sgp40/include/sgp40/sgp40.h
@@ -19,7 +19,6 @@ struct sgp40 {
 #else
     struct os_dev dev;
 #endif
-
     struct sensor sensor;
     struct sgp40_cfg cfg;
 
@@ -28,9 +27,12 @@ struct sgp40 {
 int sgp40_init(struct os_dev *, void *arg);
 int sgp40_config(struct sgp40 *sgp40, const struct sgp40_cfg *cfg);
 
+#if MYNEWT_VAL(BUS_DRIVER_PRESENT)
 int sgp40_create_i2c_sensor_dev(struct bus_i2c_node *node, const char *name,
                               const struct bus_i2c_node_cfg *i2c_cfg,
                               struct sensor_itf *sensor_itf);
+#endif
+
 #ifdef __cplusplus
 }
 #endif
